3c46aaf5ac6e084a206f5208b281f1679c0d6802
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-11 11:10:31 -0300

    exercicio 4

---
+++ atividades/exercicio4.js
@@ -0,0 +1,9 @@
+let numero = 5
+let multiplicador = 0
+
+while (multiplicador <= 9){
+    multiplicador++
+    let resultado = numero * multiplicador
+    
+    console.log(numero + "x" + multiplicador + " = " + resultado)
+}
