cc83f030000297ead56aa94041187563a775a047
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-11 10:54:28 -0300

    exercicio 3

---
+++ atividades/exercicio3.js
@@ -0,0 +1,9 @@
+let somador = 0
+let numero = 1
+
+while (numero <= 30) {
+    somador++
+    numero+= somador
+}
+console.log("Resultado: " + (somador + numero))
+console.log("Vezes somadas: " + somador)
