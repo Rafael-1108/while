299dbf6552f3f7c92f1a7a8ccb742143bf94554d
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-11 21:16:39 -0300

    exercicio 7

--- atividades/exercicio7.js
+++ atividades/exercicio7.js
@@ -1,9 +1,8 @@
 let moedas = 0;
 let reais = 0;
 
-while (reais < 12) { 
+while (reais < 12){ 
     reais += 3;
     moedas ++;
 }
-console.log("Para juntar pelo menos 12 reais é necesário pelo menos " + moedas)
-// console.log(`tem ${reais} reis com ${moedas}moedas `)
+console.log("Para juntar pelo menos " + reais + " são necessárias " + moedas + " moedas.");
